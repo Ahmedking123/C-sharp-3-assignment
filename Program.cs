58d4eae6b28988b1525d40545b1840f4fabfb95c
ahmed <ahmedmohamed3dss@gmail.com>
2026-09-15 01:11:16 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace C_sharp_3__assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
