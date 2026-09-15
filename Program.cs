eb584f2714f743b532514dd39c7d18dd362f7817
ahmed <ahmedmohamed3dss@gmail.com>
2026-09-15 06:31:13 +0300

    Twelth question

--- Program.cs
+++ Program.cs
@@ -3,18 +3,22 @@ namespace C_sharp_3__assignment
 {
     internal class Program
     {
-       
-		
+
+
         static void Main(string[] args)
         {
-            #region Eleventh question
 
-            PrintBookInfo(pages: 450,title :"Clean Code");
-            PrintBookInfo (title: "Clean Code",pages: 400);
-            
+            #region Twelfth question
+
+            PrintAllTitles("Clean Code", "The Pragmatic Programmer", "Refactoring");
         }
-        static void PrintBookInfo(string title, int pages = 300) => Console.WriteLine($"Title: {title}, Pages: {pages}");
-
-#endregion
+        static void PrintAllTitles(params string[] titles)
+        {
+            foreach (var title in titles)
+            {
+                Console.WriteLine(title);
+            }
+        } 
+            #endregion
     }
 }
